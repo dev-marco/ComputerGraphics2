d2f73ca509550516971b8645397ab902f0739516
Marco <marcoaremail@gmail.com>
2016-05-24 12:00:16 -0300

    Script to install library dependencies

---
+++ depinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+(
+    echo Installing / Upgrading packages
+    sudo apt-get update
+    sudo apt-get install libglew-dev cmake libglu1-mesa-dev freeglut3-dev mesa-common-dev git xorg-dev libgl1-mesa-dev libsdl2-dev libsdl2-mixer-dev -y
+    START_DIR=`pwd`
+    TMP_DIR=`mktemp -d`
+
+    echo Installing lastest GLFW
+    git clone --recursive https://github.com/glfw/glfw ${TMP_DIR} && \
+    cd ${TMP_DIR} && \
+    cmake . && \
+    make && \
+    sudo make install
+    cd ${START_DIR}
+    rm -fr ${TMP_DIR}
+)
