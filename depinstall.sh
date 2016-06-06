6564ca6ebccdc1efc7e08db17d0da94769fe8127
Marco <marcoaremail@gmail.com>
2016-06-06 13:52:04 -0300

    Added g++-5 to package list

--- depinstall.sh
+++ depinstall.sh
@@ -3,7 +3,11 @@
 (
     echo Installing / Upgrading packages
     sudo apt-get update
-    sudo apt-get install libglew-dev cmake libglu1-mesa-dev freeglut3-dev mesa-common-dev git xorg-dev libgl1-mesa-dev libsdl2-dev libsdl2-mixer-dev -y
+    sudo apt-get install libglew-dev cmake libglu1-mesa-dev freeglut3-dev mesa-common-dev git xorg-dev libgl1-mesa-dev libsdl2-dev libsdl2-mixer-dev software-properties-common -y
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test && \
+    sudo apt-get update && \
+    sudo apt-get install g++-5 -y
+
     START_DIR=`pwd`
     TMP_DIR=`mktemp -d`
 
