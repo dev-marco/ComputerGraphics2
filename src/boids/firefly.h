62713e7fe39ddb4450724617faa478427f7efb3e
Marco <marcoaremail@gmail.com>
2016-06-08 12:17:15 -0300

    Renamed from bird

---
+++ src/boids/firefly.h
@@ -0,0 +1,14 @@
+#ifndef SRC_BOIDS_BIRD_H_
+#define SRC_BOIDS_BIRD_H_
+
+#include "../engine/engine.h"
+
+namespace Boids {
+
+    class Firefly : public Engine::Object {
+
+    };
+    
+}
+
+#endif
