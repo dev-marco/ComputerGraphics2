372563de5ac5d9ec2a13904dc63736c00e510938
Marco <marcoaremail@gmail.com>
2016-06-13 07:09:50 -0300

    New test cases

--- src/main.cc
+++ src/main.cc
@@ -1,71 +1,11 @@
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <random>
 #include "engine/engine.h"
+#include "boids/flock.h"
 
 #define WINDOW_FPS 60
-
-
-void recursiveTriangle(
-    const Engine::Vec<3> &a,
-    const Engine::Vec<3> &b,
-    const Engine::Vec<3> &c,
-    const float_max_t radius,
-    const unsigned steps) {
-    if (steps <= 0) {
-        Engine::Draw::normal(a);
-        Engine::Draw::vertex(a[0] * radius, a[1] * radius, a[2] * radius);
-
-        Engine::Draw::normal(b);
-        Engine::Draw::vertex(b[0] * radius, b[1] * radius, b[2] * radius);
-
-        Engine::Draw::normal(c);
-        Engine::Draw::vertex(c[0] * radius, c[1] * radius, c[2] * radius);
-    } else {
-        const unsigned next_step = steps - 1;
-        const Engine::Vec<3>
-            ab = ((a + b) * 0.5).normalized(),
-            ac = ((a + c) * 0.5).normalized(),
-            bc = ((b + c) * 0.5).normalized();
-
-        recursiveTriangle( a, ab, ac, radius, next_step);
-        recursiveTriangle( b, bc, ab, radius, next_step);
-        recursiveTriangle( c, ac, bc, radius, next_step);
-        recursiveTriangle(ab, bc, ac, radius, next_step);
-    }
-}
-
-void drawsphere(unsigned steps, const Engine::Vec<3> &center = Engine::Vec<3>::zero, const float_max_t radius = 1.0) {
-    constexpr float_max_t
-        X = 0.525731112119133606,
-        Z = 0.850650808352039932;
-
-    static const Engine::Vec<3> vdata[12] = {
-        {  -X, 0.0,   Z }, {   X, 0.0,   Z }, {  -X, 0.0,  -Z }, {   X, 0.0,  -Z },
-        { 0.0,   Z,   X }, { 0.0,   Z,  -X }, { 0.0,  -Z,   X }, { 0.0,  -Z,  -X },
-        {   Z,   X, 0.0 }, {  -Z,   X, 0.0 }, {   Z,  -X, 0.0 }, {  -Z,  -X, 0.0 }
-    };
-
-    constexpr unsigned tindices[20][3] = {
-        { 0,  4,  1 }, { 0, 9,  4 }, { 9,  5, 4 }, {  4, 5, 8 }, { 4, 8,  1 },
-        { 8, 10,  1 }, { 8, 3, 10 }, { 5,  3, 8 }, {  5, 2, 3 }, { 2, 7,  3 },
-        { 7, 10,  3 }, { 7, 6, 10 }, { 7, 11, 6 }, { 11, 0, 6 }, { 0, 1,  6 },
-        { 6,  1, 10 }, { 9, 0, 11 }, { 9, 11, 2 }, {  9, 2, 5 }, { 7, 2, 11 }
-    };
-
-    Engine::Draw::push();
-
-    Engine::Draw::translate(center);
-    Engine::Draw::begin(GL_TRIANGLES);
-
-    for (const auto &indices : tindices) {
-        recursiveTriangle(vdata[indices[0]], vdata[indices[1]], vdata[indices[2]], radius, steps);
-    }
-
-    Engine::Draw::end();
-    Engine::Draw::pop();
-
-}
-
 int main (int argc, const char *argv[]) {
 
     if (!glfwInit()) {
@@ -103,33 +43,110 @@ int main (int argc, const char *argv[]) {
             program.attachFragmentShaderFile("src/engine/shaders/lighting.frag");
             program.link();
         } catch (std::string e) {
-            std::cout << e << std::endl;
+            std::cerr << e << std::endl;
             return -1;
         }
 
-        window.setShader(&program);
+        // window.setShader(&program);
 
-        window.addObject(
-            new Engine::Object(
-                Engine::Vec<3>::zero,
+        program.onAfterUse([] (Engine::Shader::Program *program) {
+            GLfloat mp[16], mv[16];
+            glGetFloatv(GL_PROJECTION_MATRIX, mp);
+            glGetFloatv(GL_MODELVIEW_MATRIX, mv);
+            glUniformMatrix4fv(program->getUniformLocationARB("scriptProj"), 1, GL_FALSE, &mp[0]);
+            glUniformMatrix4fv(program->getUniformLocationARB("scriptModelView"), 1, GL_FALSE, &mv[0]);
+            glUniform1fARB(program->getUniformLocationARB("scriptTime"), glfwGetTime());
+        });
+
+        Engine::Object obstacles(
+            { 0.0, 0.0, 0.0 },
+            Engine::Quaternion::identity
+        );
+
+        Engine::Object tower(
+            { 0.0, 0.0, 0.0 },
+            Engine::Quaternion::identity,
+            true,
+            new Engine::Cone(Engine::Vec<3>::zero, { 0.0, 0.0, 5.0 }, 0.5, 0.0, new Engine::BackgroundColor(Engine::Color::rgba(50, 50, 50, 1.0))),
+            nullptr
+        );
+
+        obstacles.addChild(&tower);
+
+        std::mt19937 random_generator(std::chrono::system_clock::now().time_since_epoch().count());
+        std::uniform_int_distribution<unsigned> random_obstacles(50, 80), random_rgb(0, 255);
+        std::uniform_int_distribution<int> random_type(0, 2);
+        std::uniform_real_distribution<float_max_t> random_xy(-10.0, 10.0), random_z(0.0, 10.0), random_radius(0.5, 1.5), random_angle(-Engine::DEG180, Engine::DEG180);
+
+        for (unsigned i = 0, total = random_obstacles(random_generator); i < total; ++i) {
+
+            Engine::Mesh *mesh = nullptr;
+            Engine::Vec<3> position = { random_xy(random_generator), random_xy(random_generator), random_z(random_generator) };
+            Engine::Background *bg = new Engine::BackgroundColor(
+                Engine::Color::rgb(random_rgb(random_generator), random_rgb(random_generator), random_rgb(random_generator))
+            );
+
+            if (position[0] < 0.0) { position[0] -= 5.0; } else { position[0] += 5.0; }
+            if (position[1] < 0.0) { position[1] -= 5.0; } else { position[1] += 5.0; }
+
+            switch (random_type(random_generator)) {
+                case 0:
+                    std::cout << "sphere" << std::endl;
+                    mesh = new Engine::Sphere3D(position, random_radius(random_generator), bg);
+                break;
+                case 1:
+                    std::cout << "cylinder" << std::endl;
+                    mesh = new Engine::Cylinder(position, position + Engine::Vec<3>::random(0.5, 3.0), random_radius(random_generator), bg);
+                break;
+                default:
+                    std::cout << "cone" << std::endl;
+                    mesh = new Engine::Cone(position, position + Engine::Vec<3>::random(0.5, 3.0), random_radius(random_generator), random_radius(random_generator), bg);
+                break;
+            }
+
+            obstacles.addChild(new Engine::Object(
+                Engine::Vec<3>::origin,
                 Engine::Quaternion::identity,
                 true,
-                new Engine::Rectangle2D({ -1.0, 0.5, 0.0 }, 4.0, 2.0),
-                nullptr,
-                new Engine::BackgroundColor(Engine::Color::rgba(50, 50, 50, 1.0))
-            )
-        );
+                mesh
+            ));
+        }
+
+        window.addObject(&obstacles);
 
         window.addObject(
             new Engine::Object(
-                { 3.0, 3.0, 0.0 },
-                Engine::Quaternion::axisAngle({ 1.0, 0.0, 0.0 }, Engine::DEG45),
+                { -250.0, 250.0, 0.0 },
+                Engine::Quaternion::identity,
                 true,
-                new Engine::Polygon2D(Engine::Vec<3>::zero, 1.0, 5),
-                nullptr,
-                new Engine::BackgroundColor(Engine::Color::rgba(50, 50, 50, 1.0))
+                new Engine::Rectangle2D(Engine::Vec<3>::zero, 5000.0, 5000.0, Engine::Quaternion::identity, new Engine::BackgroundColor(Engine::Color::rgba(255, 255, 0, 1.0))),
+                nullptr
             )
         );
+
+        window.addObject(new Boids::Flock(50, &obstacles));
+
+        // window.addObject(
+        //     new Engine::Object(
+        //         { 3.0, 3.0, 0.0 },
+        //         Engine::Quaternion::axisAngle({ 1.0, 0.0, 0.0 }, Engine::DEG45),
+        //         true,
+        //         new Engine::Polygon2D(Engine::Vec<3>::zero, 1.0, 5),
+        //         nullptr,
+        //         new Engine::BackgroundColor(Engine::Color::rgba(50, 50, 50, 1.0))
+        //     )
+        // );
+        //
+        // window.addObject(
+        //     new Engine::Object(
+        //         Engine::Vec<3>::zero,
+        //         Engine::Quaternion::identity,
+        //         true,
+        //         new Engine::Rectangle2D({ -2.0, 1.0, 0.0 }, 4.0, 2.0),
+        //         nullptr,
+        //         new Engine::BackgroundColor(Engine::Color::rgba(50, 50, 50, 1.0))
+        //     )
+        // );
 
         GLint v;
 
@@ -157,13 +174,9 @@ int main (int argc, const char *argv[]) {
 
             glMatrixMode(GL_MODELVIEW);
             glLoadIdentity();
-            gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
+            gluLookAt(15.0, 0.0, 15.0, -1.0, 0.0, -1.0, 0.0, 0.0, 1.0);
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_ACCUM_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-
-            // glColor4d(0.5, 0.5, 0.5, 1.0);
-            // drawsphere(3, { 0.0, 0.0, 0.0 }, 2.0);
 
             window.update();
             window.draw();
@@ -175,7 +188,7 @@ int main (int argc, const char *argv[]) {
 
             unsigned fps = window.sync(WINDOW_FPS);
             if (fps != WINDOW_FPS) {
-                std::cout << fps << " FPS" << std::endl;
+                // std::cout << fps << " FPS" << std::endl;
             }
         }
 
