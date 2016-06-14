e67570df62293971b1665fe22316a1ba1bda621b
Marco <marcoaremail@gmail.com>
2016-06-13 22:09:57 -0300

    Smooth averages and eye management

--- src/boids/flock.cc
+++ src/boids/flock.cc
@@ -3,11 +3,63 @@
 
 namespace Boids {
 
-    Flock::Flock (unsigned number, Engine::Object *_obstacles, const Engine::Vec<3> &_position) :
-        Engine::Object(_position, Engine::Quaternion::identity), obstacles(_obstacles) {
+    Flock::Flock (Engine::Window &_window, unsigned number, Obstacles &_obstacles, const Engine::Vec<3> &_position) :
+        Engine::Object(_position, Engine::Quaternion::identity), window(_window), obstacles(_obstacles) {
         for (unsigned i = 0; i < number; ++i) {
-            this->addChild(new Firefly());
+            static std::uniform_real_distribution<float_max_t>
+                random_angle(-Engine::DEG180, Engine::DEG180),
+                random_radius(1.0, 5.0);
+
+            const float_max_t
+                position_angle = random_angle(this->random_generator),
+                position_radius = random_radius(this->random_generator);
+
+            this->addChild(new Firefly({
+                std::cos(position_angle) * position_radius,
+                std::sin(position_angle) * position_radius,
+                random_radius(this->random_generator)
+            }));
         }
+
+        window.event<Engine::Event::Keyboard>([ this ] (GLFWwindow *window, int key, int code, int action, int mods) {
+
+            Engine::Object *leader = this->getLeader();
+
+            if (GLFW_PRESS == action) {
+                if (GLFW_KEY_Q == key) {
+                    this->window.close();
+                } else if (GLFW_KEY_V == key) {
+                    this->view_mode = static_cast<Flock::ViewMode>((static_cast<int>(view_mode) + 1) % 3);
+                } else if (GLFW_KEY_KP_ADD == key || GLFW_KEY_EQUAL == key) {
+                    this->addChild(new Firefly(this->averagePosition() + Engine::Vec<3>::random(-1.0, 1.0)));
+                } else if (GLFW_KEY_MINUS == key || GLFW_KEY_KP_SUBTRACT == key) {
+                    Engine::Object *last = this->getChildren().back();
+                    this->removeChild(last);
+                    last->destroy();
+                }
+            }
+            if (GLFW_PRESS == action || GLFW_REPEAT == action) {
+                if (leader != nullptr) {
+                    if (GLFW_KEY_W == key) {
+                        leader_angle_z = std::max(leader_angle_z - 0.02, leader_min_angle_z);
+                        leader_angle_z = std::atan2(std::sin(leader_angle_z), std::cos(leader_angle_z));
+                    } else if (GLFW_KEY_S == key) {
+                        leader_angle_z = std::min(leader_angle_z + 0.02, leader_max_angle_z);
+                        leader_angle_z = std::atan2(std::sin(leader_angle_z), std::cos(leader_angle_z));
+                    } else if (GLFW_KEY_D == key) {
+                        leader_angle_y = std::max(leader_angle_y - 0.02, leader_min_angle_y);
+                        leader_angle_y = std::atan2(std::sin(leader_angle_y), std::cos(leader_angle_y));
+                    } else if (GLFW_KEY_A == key) {
+                        leader_angle_y = std::min(leader_angle_y + 0.02, leader_max_angle_y);
+                        leader_angle_y = std::atan2(std::sin(leader_angle_y), std::cos(leader_angle_y));
+                    } else if (GLFW_KEY_UP == key) {
+                        leader_speed = std::min(leader_speed + 0.02, leader_max_speed);
+                    } else if (GLFW_KEY_DOWN == key) {
+                        leader_speed = std::max(leader_speed - 0.02, leader_min_speed);
+                    }
+                }
+            }
+        }, "keyboard.controls");
     }
 
 };
--- src/boids/flock.h
+++ src/boids/flock.h
@@ -2,59 +2,118 @@
 #define SRC_BOIDS_FLOCK_H_
 
 #include <set>
+#include <random>
+#include <chrono>
+#include "obstacles.h"
 #include "../engine/engine.h"
 
 namespace Boids {
 
+    constexpr float_max_t
+        leader_max_angle_y =  Engine::DEG180,
+        leader_min_angle_y = -Engine::DEG180,
+        leader_max_angle_z =  Engine::DEG45,
+        leader_min_angle_z = -Engine::DEG45,
+        leader_min_speed = 0.5,
+        leader_max_speed = 2.0,
+        leader_start_speed = (leader_min_speed + leader_min_speed) / 2.0;
+
     class Flock : public Engine::Object {
 
-        Engine::Object *obstacles;
+    public:
+
+        enum ViewMode : int {
+            VIEW_TOWER, VIEW_BACK, VIEW_SIDE
+        };
+
+    private:
+
+        static constexpr unsigned logs_maxsize = 50;
+        ViewMode view_mode = ViewMode::VIEW_TOWER;
+        Engine::Window &window;
+        Obstacles &obstacles;
+        Engine::Object *leader = nullptr;
         std::set<Engine::Object *> fireflies;
-        bool values_set = false;
-        Engine::Vec<3> avg_position, avg_speed, avg_acceleration;
-        float_max_t min_x, min_y, min_z, max_x, max_y, max_z;
+        bool values_set = false, smooth_values_set = false;
+        Engine::Vec<3> avg_position, avg_speed, avg_acceleration, smooth_avg_position, smooth_avg_speed;
+        std::list<Engine::Vec<3>> position_log, speed_log;
+        float_max_t min_x, min_y, min_z, max_x, max_y, max_z, leader_angle_z = 0.0, leader_angle_y = 0.0, leader_speed = leader_start_speed;
+        std::mt19937 random_generator = std::mt19937(std::chrono::system_clock::now().time_since_epoch().count());
 
-        void setAverages (void) {
-            this->avg_position = this->avg_speed = this->avg_acceleration = Engine::Vec<3>::zero;
-            this->min_x = this->min_y = this->min_z =  std::numeric_limits<float_max_t>::infinity();
-            this->max_x = this->max_y = this->max_z = -std::numeric_limits<float_max_t>::infinity();
+        void setAverages (bool force = false) {
+            if (!this->values_set || force) {
+                this->avg_position = this->avg_speed = this->avg_acceleration = Engine::Vec<3>::zero;
+                this->min_x = this->min_y = this->min_z =  std::numeric_limits<float_max_t>::infinity();
+                this->max_x = this->max_y = this->max_z = -std::numeric_limits<float_max_t>::infinity();
 
-            if (this->fireflies.size()) {
+                if (this->fireflies.size() > 1) {
 
-                for (const auto &firefly : this->fireflies) {
-                    static Engine::Vec<3> position = firefly->getPosition();
-                    this->min_x = std::min(this->min_x, position[0]);
-                    this->min_y = std::min(this->min_y, position[1]);
-                    this->min_z = std::min(this->min_z, position[2]);
-                    this->max_x = std::max(this->max_x, position[0]);
-                    this->max_y = std::max(this->max_y, position[1]);
-                    this->max_z = std::max(this->max_z, position[2]);
-                    this->avg_position += position;
-                    this->avg_speed += firefly->getSpeed();
-                    this->avg_acceleration += firefly->getAcceleration();
+                    unsigned computed = this->fireflies.size() - 1;
+
+                    for (const auto &firefly : this->fireflies) {
+                        if (firefly != this->getLeader()) {
+                            static Engine::Vec<3> position;
+                            position = firefly->getPosition();
+                            this->min_x = std::min(this->min_x, position[0]);
+                            this->min_y = std::min(this->min_y, position[1]);
+                            this->min_z = std::min(this->min_z, position[2]);
+                            this->max_x = std::max(this->max_x, position[0]);
+                            this->max_y = std::max(this->max_y, position[1]);
+                            this->max_z = std::max(this->max_z, position[2]);
+                            this->avg_position += position;
+                            this->avg_speed += firefly->getSpeed();
+                            this->avg_acceleration += firefly->getAcceleration();
+                        }
+                    }
+
+                    this->avg_position /= computed;
+                    this->avg_speed /= computed;
+                    this->avg_acceleration /= computed;
+
+                    this->position_log.push_back(avg_position);
+                    this->speed_log.push_back(avg_speed);
+
+                    if (this->position_log.size() > logs_maxsize) {
+                        this->position_log.pop_front();
+                        this->speed_log.pop_front();
+                    }
+
+                    this->smooth_values_set = false;
+                    this->values_set = true;
                 }
+            }
+        }
 
-                this->avg_position /= this->fireflies.size();
-                this->avg_speed /= this->fireflies.size();
-                this->avg_acceleration /= this->fireflies.size();
-
-                this->values_set = true;
+        void setSmoothAverages (bool force = false) {
+            this->setAverages();
+            if (!this->smooth_values_set || force) {
+                this->smooth_avg_position = Engine::Vec<3>::average(this->position_log);
+                this->smooth_avg_speed = Engine::Vec<3>::average(this->speed_log);
+                this->smooth_values_set = true;
             }
         }
 
     public:
 
-        Flock (unsigned number, Engine::Object *_obstacles, const Engine::Vec<3> &_position = Engine::Vec<3>::zero);
+        Flock (Engine::Window &_window, unsigned number, Obstacles &_obstacles, const Engine::Vec<3> &_position = Engine::Vec<3>::zero);
 
         const std::set<Engine::Object *> &getBoids (void) const { return this->fireflies; }
-        const std::list<Engine::Object *> &getObstacles (void) const { return this->obstacles->getChildren(); }
+        const std::list<Engine::Object *> &getObstacles (void) const { return this->obstacles.getChildren(); }
 
         void afterUpdate (float_max_t now, float_max_t delta_time, unsigned tick) override {
+            Engine::Object *leader = this->getLeader();
             this->values_set = false;
+            if (leader) {
+                leader->setSpeed(Engine::Quaternion::eulerZYX(this->leader_angle_z, this->leader_angle_y, 0.0).rotated(Engine::Vec<3>::axisX) * this->leader_speed);
+            }
         }
 
         void onAddChild (Engine::Object *child) override {
             if (child->getType() == "firefly") {
+                if (this->leader == nullptr) {
+                    this->leader = child;
+                    child->setSpeed(Engine::Vec<3>({ 1.0, 1.0, 1.0 }).normalized());
+                }
                 this->fireflies.insert(child);
                 this->values_set = false;
             }
@@ -67,40 +126,97 @@ namespace Boids {
             }
         }
 
-        Engine::Vec<3> averagePosition (void) {
-            if (!this->values_set) {
-                this->setAverages();
+        Engine::Vec<3> eyePos (void) {
+
+            if (this->view_mode == ViewMode::VIEW_TOWER) {
+                return { 0.0, 0.0, this->obstacles.getTowerHeight() };
+            } else {
+
+                const Engine::Vec<3>
+                    &flock_center = this->smoothAveragePosition(),
+                    &flock_speed = this->smoothAverageSpeed();
+                Engine::Vec<3> eye_pos;
+
+                if (this->view_mode == ViewMode::VIEW_BACK) {
+                    Engine::Mesh::intersectionRayBox(
+                        flock_center,
+                        flock_center + flock_speed,
+                        { this->minX(), this->minY(), this->minZ() },
+                        { this->maxX(), this->maxY(), this->maxZ() },
+                        eye_pos
+                    );
+                } else {
+                    Engine::Mesh::intersectionRayBox(
+                        flock_center,
+                        flock_center + flock_speed.cross(Engine::Vec<3>::axisZ),
+                        { this->minX(), this->minY(), this->minZ() },
+                        { this->maxX(), this->maxY(), this->maxZ() },
+                        eye_pos
+                    );
+                }
+
+                return eye_pos + (eye_pos - flock_center).normalized();
             }
+        }
+
+        Engine::Object *getLeader (void) const { return this->leader; }
+
+        Engine::Vec<3> averagePosition (void) {
+            this->setAverages();
             return this->avg_position;
         }
 
+        Engine::Vec<3> smoothAveragePosition (void) {
+            this->setSmoothAverages();
+            return this->smooth_avg_position;
+        }
+
         Engine::Vec<3> averageSpeed (void) {
-            if (!this->values_set) {
-                this->setAverages();
-            }
-            return this->avg_speed;
+            this->setSmoothAverages();
+            return this->smooth_avg_speed;
+        }
+
+        Engine::Vec<3> smoothAverageSpeed (void) {
+            this->setAverages();
+            return Engine::Vec<3>::average(this->speed_log);
         }
 
         Engine::Vec<3> averageAcceleration (void) {
-            if (!this->values_set) {
-                this->setAverages();
-            }
+            this->setAverages();
             return this->avg_acceleration;
         }
 
-        Engine::Vec<3> minXYZ (void) {
-            if (!this->values_set) {
-                this->setAverages();
-            }
-            return { this->min_x, this->min_y, this->min_z };
+        float_max_t minX (void) {
+            this->setAverages();
+            return this->min_x;
         }
 
-        Engine::Vec<3> maxXYZ (void) {
-            if (!this->values_set) {
-                this->setAverages();
-            }
-            return { this->max_x, this->max_y, this->max_z };
+        float_max_t minY (void) {
+            this->setAverages();
+            return this->min_y;
         }
+
+        float_max_t minZ (void) {
+            this->setAverages();
+            return this->min_z;
+        }
+
+        float_max_t maxX (void) {
+            this->setAverages();
+            return this->max_x;
+        }
+
+        float_max_t maxY (void) {
+            this->setAverages();
+            return this->max_y;
+        }
+
+        float_max_t maxZ (void) {
+            this->setAverages();
+            return this->max_z;
+        }
+
+        inline std::string getType () const override { return "flock"; }
 
     };
 
